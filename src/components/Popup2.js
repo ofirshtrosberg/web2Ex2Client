d407e9ba1bfb7f6b70534715746e2a1d9cddb946
Yarden Messika <yardenm96@gmail.com>
2022-12-13 20:05:27 +0200

    submit works and deletes cart

--- src/components/Popup2.js
+++ src/components/Popup2.js
@@ -2,16 +2,30 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 function Popup2(props){
+
+    const submitHandler = (event) => {
+        console.log("#")
+
+        event.preventDefault();
+    
+        const expenseData = {
+        };
+        // console.log(expenseData);
+    
+        fetch(
+          "http://localhost:3000/userProducts"
+        ).catch((err) => console.log(err));
+      };
     return (props.trigger) ? (
         <div className="popup text-center">
             <div className="popup-inner">
-                <Button className="close-btn btn-light" onClick={() => props.setTrigger(false)}> Close</Button>
+                <Button className="close-btn btn-light" onClick={() => props.setTrigger(false)}>Close</Button>
                 <div className="ProductInfo">
                 
       <br></br>
 
-    <form action="/action_page.php" class="form-container">
-        <h1>Payment Info</h1>
+      <form onSubmit={submitHandler}>        
+      <h1>Payment Info</h1>
 
         <label for="cname">Name on Card</label>
             <input type="text" id="cname" name="cardname" placeholder="John More Doe"></input>
@@ -31,40 +45,8 @@ function Popup2(props){
                 <label for="cvv">CVV</label>
                 <input type="text" id="cvv" name="cvv" placeholder="352"></input>
 
-    {/* <div class="row"> */}
-          {/* <div class="col-50">
-            <h3>Billing Address</h3>
-            <br></br>
+        <button type="submit" class="btn">Submit</button>
 
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <br></br>
-
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe"></input>
-            <br></br>
-
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <br></br>
-
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"></input>
-            <br></br>
-
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <br></br>
-
-            <input type="text" id="city" name="city" placeholder="New York"></input>
-        </div>
-    </div> */}
-
-    {/* <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018"></input>
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label></input>
-                <input type="text" id="cvv" name="cvv" placeholder="352"></input>
-              </div>
-        </div>   */}
     </form>
 
 
@@ -74,7 +56,7 @@ function Popup2(props){
       <br></br>
     </div>
 
-            </div>
+    </div>
     ): "";
 }
 
